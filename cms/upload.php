6262ae15e1916afbb622a546c54de568ef77bd57
James Nelhams <james.nelhams@gmail.com>
2019-01-14 15:57:20 +0000

    SQL in upload

--- cms/upload.php
+++ cms/upload.php
@@ -2,6 +2,12 @@
 include("../includes/functions.inc.php");
 include("../includes/conn.inc.php");
 
+$sql = "SELECT MAX(objectId)AS Max FROM objects";
+$stmt->prepare($sql);
+$stmt->execute();
+$row->fetchObject();
+echo $row->Max;
+
 $currentDir = getcwd();
 $uploadDirectory = "../CONTENT/IMAGES/";
 mkdir($uploadDirectory . "memez");
