e9c861023b01ff3280697ac625c0771316fdc314
James Nelhams <james.nelhams@gmail.com>
2019-01-14 10:21:22 +0000

    Background added

--- index.php
+++ index.php
@@ -9,17 +9,18 @@ include("includes/functions.inc.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- Stylesheet for Mobile -->
-    <link rel="stylesheet" type="text/css" href="css/mobile.css">
     <!-- Stylesheet for Desktop -->
-    <link rel="stylesheet" media="only screen and (min-width: 901px)" href="css/desktop.css">
+    <link rel="stylesheet" href="css/desktop.css">
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
     <!-- Favicon -->
     <link rel="icon" href="content/images/favicon.png">
-    <img src="content/images/logo.png" alt="Kelham Island Logo">
     <title>Kelham Island Web</title>
 </head>
 <body>
+    <div style="height: 600px; width: 100%">
+    <img class="indexLogo" src="content/images/logo.png" alt="Kelham Island Logo">
+
+    </div>
     <div class="container">
         <h1>Hello World</h1>
         <a href="objectSelect.php" class="buttonGo">Start</a>
