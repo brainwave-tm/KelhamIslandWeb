7f510f2819c9938e2bd60c33a87d4fed4ec7db3c
hdeo12345 <36956061+hdeo12345@users.noreply.github.com>
2019-01-14 09:54:31 +0000

    single-object page added

--- single-object.php
+++ single-object.php
@@ -4,5 +4,6 @@ include("includes/functions.inc.php");
 
 $objectID = safeString($_GET['objectID']);
 
-$objectData = "SELECT * FROM objects WHERE objectID = ".$objectID;
+$objectData = $pdo->query("SELECT * FROM objects WHERE objectID = $objectID")->fetchObject();
+echo $objectData->objectName;
 ?>
