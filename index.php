4346fd6ce6897c0c1361d500b97f23fb8775010c
James Nelhams <james.nelhams@gmail.com>
2019-01-14 09:40:37 +0000

    Object selection button
    Object selection button added and styled

--- index.php
+++ index.php
@@ -2,12 +2,16 @@
 
 ?>
 <!DOCTYPE html>
-<!-- Stylesheet for Mobile -->
-<link rel="stylesheet" type="text/css" href="css/mobile.css">
-<!-- Stylesheet for Desktop -->
-<link rel="stylesheet" media="only screen and (min-width: 901px)" href="css/desktop.css">
-<!-- Favicon -->
-<link rel="icon" href="CONTENT/IMAGES/FAVICON.png">
 <html>
-    
+<head>
+    <!-- Stylesheet for Mobile -->
+    <link rel="stylesheet" type="text/css" href="css/mobile.css">
+    <!-- Stylesheet for Desktop -->
+    <link rel="stylesheet" media="only screen and (min-width: 901px)" href="css/desktop.css">
+    <!-- Favicon -->
+    <link rel="icon" href="CONTENT/IMAGES/FAVICON.png">  
+</head>
+<body>
+    <a href="objectSelect.html" class="buttonGo">Start</a>
+</body>
 </html>
