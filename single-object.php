ff1ec6250e4cebe82e9efdfe9b7d71f8c269a7a6
Jake Stringer <b7003087@my.shu.ac.uk>
2019-01-14 10:14:25 +0000

    Replaced logo

--- single-object.php
+++ single-object.php
@@ -4,6 +4,10 @@ include("includes/functions.inc.php");
 
 $objectID = safeString($_GET['objectID']);
 
-$objectData = $pdo->query("SELECT * FROM objects WHERE objectID = $objectID")->fetchObject();
-echo $objectData->objectName;
+$objectData = $pdo->query("SELECT * FROM objects 
+                           INNER JOIN images ON objects.objectId = images.imageId
+                           WHERE objectID = $objectID")->fetchObject();
+echo "<h1>$objectData->objectName</h1>";
+echo "<img src='$objectData->imageUrl' title='$objectData->imageDescription' alt='$objectData->imageDescription' style='width: 25%;'>";
+echo "<p>$objectData->objectLongDescription</p>";
 ?>
