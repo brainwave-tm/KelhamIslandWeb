a3dd22f3f4d7bd9701c64e8cb22512a43e514a06
Jake Stringer <b7003087@my.shu.ac.uk>
2019-01-14 09:39:09 +0000

    Added includes and page template

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
 <?php
-
+include("includes/conn.inc.php");
+include("includes/functions.inc.php");
 ?>
+
 <!DOCTYPE html>
-<!-- Stylesheet for Mobile -->
-<link rel="stylesheet" type="text/css" href="css/mobile.css">
-<!-- Stylesheet for Desktop -->
-<link rel="stylesheet" media="only screen and (min-width: 901px)" href="css/desktop.css">
-<!-- Favicon -->
-<link rel="icon" href="CONTENT/IMAGES/FAVICON.png">
-<html>
-    
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Stylesheet for Mobile -->
+    <link rel="stylesheet" type="text/css" href="css/mobile.css">
+    <!-- Stylesheet for Desktop -->
+    <link rel="stylesheet" media="only screen and (min-width: 901px)" href="css/desktop.css">
+    <!-- Favicon -->
+    <link rel="icon" href="content/images/favicon.png">
+    <title>Kelham Island Web</title>
+</head>
+<body>
+    <h1>Hello World</h1>
+</body>
 </html>
