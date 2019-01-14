6f3c2074fec52a8a9c1c1cd7a120bb26c232c741
hdeo12345 <36956061+hdeo12345@users.noreply.github.com>
2019-01-14 09:35:54 +0000

    styleseet added

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<!-- Stylesheet for Mobile -->
+<link rel="stylesheet" type="text/css" href="css/mobile.css">
+<!-- Stylesheet for Desktop -->
+<link rel="stylesheet" media="only screen and (min-width: 901px)" href="css/desktop.css">
+<!-- Favicon -->
+<link rel="icon" href="CONTENT/IMAGES/FAVICON.png">
+<html>
+    
+</html>
