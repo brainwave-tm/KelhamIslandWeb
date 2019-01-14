6baf98fb571c753fcc044d0f53021b80cbc1f1e2
Jonathan <b6013546@my.shu.ac.uk>
2019-01-14 10:41:15 +0000

    Added login form

---
+++ LoginForm.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Kelham island - Login</title>
+<link rel="stylesheet" href="css/LoginStyle.css">
+</head>
+<body>
+<div id="container">
+<header>
+  <h1></h1>
+</header>
+<section>
+<form method="post" action="">
+ <fieldset>
+        <legend></legend>
+            <p>
+                <label for="username">Username</label>
+                <input type="text" name="username" id="username" />   
+            </p>
+            <p>
+                <label for="pass">Password:</label>
+                <input type="password" id="pass" name="password" required>
+            </p>  
+            <p class="right">
+    	        <input type="submit" value="Send" class="sendButton" />
+            </p>
+    </fieldset>
+
+
+</form>
+</section>
+<footer>
+</footer>
+</div>
+</body>
+</html>
