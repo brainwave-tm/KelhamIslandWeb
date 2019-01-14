3821b3b4d1b5426094921804e78045e5e910f659
hdeo12345 <36956061+hdeo12345@users.noreply.github.com>
2019-01-14 15:50:56 +0000

    Upload started

---
+++ cms/upload.php
@@ -0,0 +1,11 @@
+<?php
+include("../includes/functions.inc.php");
+include("../includes/conn.inc.php");
+
+$currentDir = getcwd();
+$uploadDirectory = "../CONTENT/IMAGES/";
+mkdir($uploadDirectory . "memez");
+
+
+
+?>
