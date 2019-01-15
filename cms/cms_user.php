f28a383e888a73fd1149b2d4cc80f8099368c926
Konstantinos98 <37187693+Konstantinos98@users.noreply.github.com>
2019-01-15 09:08:13 +0000

    Almost done with cms user

--- cms/cms_user.php
+++ cms/cms_user.php
@@ -1,6 +1,33 @@
 <?php
 include("../includes/conn.inc.php");
 include("../includes/functions.inc.php");
+
+if(isset($_POST['username']))
+{
+    $username = $_POST['username'];
+    if (empty($username))
+    {
+        $errorUsername = 'Usernamer field is empty.';
+    }
+    
+}
+if (isset($_POST['repassword']))
+{
+    $repassword = $_POST['repassword'];
+    if (empty($repassword))
+    {
+        $errorRePassword = 'Password confirmation field is empty.';
+    }
+}
+if (isset($_POST['password']))
+{    
+    $password = $_POST['password'];
+    if(empty($password))
+    {
+        $errorPassword = 'Password field is empty.';
+    }
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -22,32 +49,52 @@ include("../includes/functions.inc.php");
         <a href="index.php"><img class="headerLogo" src="../content/images/logo.png" alt="Kelham Island Logo"></a>
         <h1>Edit your account</h1>
     </header>
-<form action="process/userNameValidation.php" method="post" >
+<form action="cms_user.php" method="post" >
     <div class="username_box">
         <h1>Change UserName</h1>
+        
         <?php
-            $getUserName = $pdo->query("SELECT userName FROM users")->fetchObject();
-            echo "Current UserName : " . $getUserName->userName;
+            if(isset($errorUsername))
+            { ?>
+            <small style="color:#aa0000;"><?php echo $errorUsername; ?></small>
+                <br /><br />
+        <?php } ?>
+
+        <?php
+            $getUserName = $pdo->query("SELECT userName FROM users")->fetchObject();            
+            echo "Current UserName : " . $getUserName->userName;            
         ?>
+
         <p>                  
             <label for="username">Enter your new Username:</label>
-            <input type="text" name="new_username" id="username" /> 
+            <input type="text" name="username" placeholder="Username" /> 
         </p>
         <p>
             <input type="submit" value="Change Username" class="changeButton" />
         </p>
     </div>
 </form>
-<form action="process/userPasswordValidation.php" method="post" >
+<form action="cms_user.php" method="post" >
     <div class="password_box">
-    <h1>Change Password</h1>    
+    <h1>Change Password</h1>   
+        
+        <?php if(isset($errorPassword)) { ?>
+            <small style="color:#aa0000;"><?php echo $errorPassword; ?></small>
+            <br />
+        <?php } ?>
+
+        <?php if(isset($errorRePassword)) { ?>
+            <small style="color:#aa0000;"><?php echo $errorRePassword; ?></small>
+            <br />
+        <?php } ?>
+
         <p>              
             <label for="password">Enter your new Password:</label>
-            <input type="text" name="new_password" id="password" />
+            <input type="text" name="password" placeholder="Password" />
         </p>
         <p>
             <label for="password">Confirm your new Password:</label>
-            <input type="text" name="new_password" id="password" />
+            <input type="text" name="repassword" placeholder="Re-enter Password" />
         </p> 
         <p >
             <input type="submit" value="Change Password" class="changeButton" />
