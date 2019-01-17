f59d1e6aa2c31ee575c62d14452987ca26e38d23
Jake Stringer <b7003087@my.shu.ac.uk>
2019-01-17 09:23:18 +0000

    icons and stuff

--- cms/editObject.php
+++ cms/editObject.php
@@ -69,8 +69,8 @@ WHERE objectId = $objectID")->fetchObject();
     </div>
 
     <div class="pageContent">
-        <h3><a href="editPages.php?objectId=<?php echo $object->objectId; ?>">Edit this object's pages</a></h3>
-        <h3><a href="addPages.php?objectId=<?php echo $object->objectId; ?>">Add pages to this object</a></h3>
+        <h3><a href="editPages.php?objectId=<?php echo $object->objectId; ?>"><i class='fas fa-pen'></i> Edit this object's pages</a></h3>
+        <h3><a href="addPages.php?objectId=<?php echo $object->objectId; ?>"><i class="fas fa-plus"></i> Add pages to this object</a></h3>
     </div>
     </fieldset>
 
